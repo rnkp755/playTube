70ea57134390ada499c4c13deb23a2d85208e5d0
Raushan Kumar Thakur <rnkp755@gmail.com>
2025-07-30 15:36:31 +0530

    Updated SERVER_URL

--- server/src/controllers/ai.controller.js
+++ server/src/controllers/ai.controller.js
@@ -50,7 +50,6 @@ const performSimilaritySearch = async (query, videoId) => {
 };
 
 const askToGemini = async (query) => {
-    console.log(query);
     const response = await ai.models.generateContent({
         model: "gemini-1.5-pro",
         contents: query,
