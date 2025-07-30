f1b4533c265553852ccc0c108d272be96fe4c1fa
Raushan Kumar Thakur <rnkp755@gmail.com>
2025-07-30 15:51:09 +0530

    Fix CORS

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://vidshare.raushan.info/"],
+        origin: ["http://localhost:5173", "https://vidshare.raushan.info"],
         credentials: true,
     })
 );
